8e5ce80f6cc1002a190dc1c27e7170d7925f5cf1
dtaleme <kiwi0tails@gmail.com>
2023-04-22 08:31:09 +0300

    kutuev

--- 01_hello/src/main.cpp
+++ 01_hello/src/main.cpp
@@ -6,34 +6,38 @@ using namespace std;
 using namespace sycl;
 
 int main() {
-    vector<platform> platforms = platform::get_platforms();
-    for (size_t i = 0; i < platforms.size(); i++) {
-        cout << "Platform №" << i << ": " << platforms[i].get_info<info::platform::name>() << "\n";
-        vector<device> devices = platforms[i].get_devices();
-        for (size_t j = 0; j < devices.size(); j++) {
-            cout << "\tDevice №" << j << ": " << devices[j].get_info<info::device::name>() << "\n";
-        }
+  cout << "  Lab1\n\n";
+  vector<platform> platforms = platform::get_platforms();
+  for (size_t i = 0; i < platforms.size(); i++) {
+    cout << "Platform " << i << ": "
+         << platforms[i].get_info<info::platform::name>() << "\n";
+    vector<device> devices = platforms[i].get_devices();
+    for (size_t j = 0; j < devices.size(); j++) {
+      cout << "  Device " << j << ": "
+           << devices[j].get_info<info::device::name>() << "\n";
+    }
+  }
+  cout << "\n";
+
+  constexpr int globalSize = 8;
+  for (size_t i = 0; i < platforms.size(); i++) {
+    vector<device> devices = platforms[i].get_devices();
+    for (size_t j = 0; j < devices.size(); j++) {
+      cout << devices[j].get_info<info::device::name>() << "\n";
+      try {
+        queue q(devices[j]);
+        q.submit([&](handler &h) {
+          stream out(1024, 80, h);
+          h.parallel_for(range<1>(globalSize), [=](id<1> item) {
+            out << "  [" << item.get(0) << "] Hello from platform " << i
+                << " and device " << j << "\n";
+          });
+        });
+        q.wait();
+      } catch (std::exception e) {
+        cout << e.what() << "\n";
+      }
     }
     cout << "\n";
-
-    constexpr int globalSize = 5;
-    for (size_t i = 0; i < platforms.size(); i++) {
-        vector<device> devices = platforms[i].get_devices();
-        for (size_t j = 0; j < devices.size(); j++) {
-            cout << devices[j].get_info<info::device::name>() << "\n";
-            try {
-                queue q(devices[j]);
-                q.submit([&](handler &h) {
-                    stream out(1024, 80, h);
-                    h.parallel_for(range<1>(globalSize), [=](id<1> item) {
-                        out << '[' << item.get(0) << "] Hello from platform " << i << " and device " << j << "\n";
-                    });
-                });
-                q.wait();
-            } catch (std::exception e) {
-                cout << e.what() << "\n";
-            }
-        }
-        cout << "\n";
-    }
+  }
 }
--- 02_integral/src/main.cpp
+++ 02_integral/src/main.cpp
@@ -1,67 +1,77 @@
+#include <CL/sycl.hpp>
 #include <cmath>
 #include <cstdlib>
 #include <iostream>
 #include <numeric>
 #include <string_view>
 
-#include <CL/sycl.hpp>
-
 #include "../include/utils.hpp"
 
 int main(int argc, char *argv[]) {
-    if (argc != 3) {
-        std::cout << "Wrong arguments passed. Exiting." << std::endl;
-        return 1;
-    }
+  if (argc != 3) {
+    std::cout << "Wrong arguments passed. Exiting." << std::endl;
+    return 1;
+  }
 
-    int stepsCount = std::atoi(argv[1]);
-    std::string_view deviceType = argv[2];
+  std::cout << "  Lab2\n\n";
 
-    constexpr size_t groupSize = 16;
-    float expected = 2.f * std::pow(std::sin(0.5f), 2.f) * std::sin(1.f);
-    float dx = 1.f / stepsCount;
-    float dy = 1.f / stepsCount;
+  int stepsCount = std::atoi(argv[1]);
+  std::string_view deviceType = argv[2];
 
-    size_t groupsCount = stepsCount / groupSize + 1;
-    std::vector<float> result(groupsCount * groupsCount, 0);
+  constexpr size_t groupSize = 16;
+  float expected = 2.f * std::pow(std::sin(0.5f), 2.f) * std::sin(1.f);
+  float dx = 1.f / stepsCount;
+  float dy = 1.f / stepsCount;
 
-    sycl::queue queue = utils::createDeviceQueueByType(deviceType);
+  size_t groupsCount = stepsCount / groupSize + 1;
+  std::vector<float> result(groupsCount * groupsCount, 0);
 
-    std::cout << "Size: " << stepsCount << " x " << stepsCount << std::endl;
-    std::cout << "Device: " << queue.get_device().get_info<sycl::info::device::name>() << std::endl << std::endl;
+  sycl::queue queue = utils::createDeviceQueueByType(deviceType);
 
-    uint64_t start = 0;
-    uint64_t end = 0;
+  std::cout << "Size - " << stepsCount << " x " << stepsCount << std::endl;
+  std::cout << "Device - "
+            << queue.get_device().get_info<sycl::info::device::name>()
+            << std::endl
+            << std::endl;
 
-    try {
-        sycl::buffer<float> resultBuffer(result.data(), result.size());
+  uint64_t start = 0;
+  uint64_t end = 0;
 
-        sycl::event event = queue.submit([&](sycl::handler &h) {
-            auto buffer = resultBuffer.get_access<sycl::access::mode::write>(h);
-            h.parallel_for(
-                sycl::nd_range<2>(sycl::range<2>(stepsCount, stepsCount), sycl::range<2>(groupSize, groupSize)),
-                [=](sycl::nd_item<2> item) {
-                    float x = dx * (item.get_global_id(0) + 0.5);
-                    float y = dy * (item.get_global_id(1) + 0.5);
-                    float value = sycl::sin(x) * sycl::cos(y);
-                    float sum = sycl::reduce_over_group(item.get_group(), value, std::plus<float>());
-                    if (item.get_local_id(0) == 0 && item.get_local_id(1) == 0) {
-                        buffer[item.get_group(0) * item.get_group_range(0) + item.get_group(1)] = sum;
-                    }
-                });
-        });
-        queue.wait();
+  try {
+    sycl::buffer<float> resultBuffer(result.data(), result.size());
 
-        start = event.get_profiling_info<sycl::info::event_profiling::command_start>();
-        end = event.get_profiling_info<sycl::info::event_profiling::command_end>();
-    } catch (std::exception &e) {
-        std::cout << e.what() << std::endl;
-    }
+    sycl::event event = queue.submit([&](sycl::handler &h) {
+      auto buffer = resultBuffer.get_access<sycl::access::mode::write>(h);
+      h.parallel_for(
+          sycl::nd_range<2>(sycl::range<2>(stepsCount, stepsCount),
+                            sycl::range<2>(groupSize, groupSize)),
+          [=](sycl::nd_item<2> item) {
+            float x = dx * (item.get_global_id(0) + 0.5);
+            float y = dy * (item.get_global_id(1) + 0.5);
+            float value = sycl::sin(x) * sycl::cos(y);
+            float sum = sycl::reduce_over_group(item.get_group(), value,
+                                                std::plus<float>());
+            if (item.get_local_id(0) == 0 && item.get_local_id(1) == 0) {
+              buffer[item.get_group(0) * item.get_group_range(0) +
+                     item.get_group(1)] = sum;
+            }
+          });
+    });
+    queue.wait();
 
-    float computed = std::accumulate(result.begin(), result.end(), 0.f) * dx * dy;
+    start =
+        event.get_profiling_info<sycl::info::event_profiling::command_start>();
+    end = event.get_profiling_info<sycl::info::event_profiling::command_end>();
+  } catch (std::exception &e) {
+    std::cout << e.what() << std::endl;
+  }
 
-    std::cout << "Kernel time: " << (end - start) / 1e+6 << " ms" << std::endl;
-    std::cout << "Expected: " << expected << std::endl;
-    std::cout << "Computed: " << computed << std::endl;
-    std::cout << "Error: " << std::abs(computed - expected) << std::endl;
+  float computed = std::accumulate(result.begin(), result.end(), 0.f) * dx * dy;
+
+  std::cout << "  Elapsed time - " << (end - start) / 1e+6 << " ms"
+            << std::endl;
+  std::cout << "  Expected val - " << expected << std::endl;
+  std::cout << "  Computed val - " << computed << std::endl;
+  std::cout << "  Error        - " << std::abs(computed - expected)
+            << std::endl;
 }
